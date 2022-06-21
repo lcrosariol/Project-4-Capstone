da520606051862e907e29bd5070dab0ab6c4c87f
lauren <laurencrosariol@yahoo.com>
2022-06-21 02:05:46 -0700

    fix bee

--- src/pages/VirtualGarden/VirtualGarden.jsx
+++ src/pages/VirtualGarden/VirtualGarden.jsx
@@ -183,27 +183,6 @@ export default function VirtualGarden() {
                     </div>
                 </div>
             </DndProvider>
-            <div id="bee">
-          {/* <!-- code snippet from: https://codepen.io/storkhuset/pen/RwKYNGJ --> */}
-            <div id="root"></div>
-                <div className="container">
-                    <div className="wings front">
-                    <div className="wing w1"></div>
-                    <div className="wing w2"></div>
-                </div>
-                    <div className="body">
-                    <div className="stripe"></div>
-                    <div className="stripe"></div>
-                    <div className="eyes">
-                        <div className="eye"><div className="pupil"></div></div>
-                        <div className="eye"><div className="pupil"></div></div>
-                    </div>
-                    <div className="mouth"></div>
-                    </div>
-                    <div className="gadd"></div>
-            </div>
-            {/* <!-- end of code snippet from: https://codepen.io/storkhuset/pen/RwKYNGJ --> */}
-        </div>
     </div>
     );
 }
